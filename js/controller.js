a751f9b6c3e81703f45b00a8af13e4e0070a03b9
woundeddophin <justfrznh2o@gmail.com>
2015-03-17 13:10:28 -0700

    fixed mixed up axis

--- js/controller.js
+++ js/controller.js
@@ -13,7 +13,7 @@
 
   function drawChart() {
 	    // Create the chart that will be used to animate year-by-year information.
-	    var chart = new google.visualization.AreaChart(document.getElementById('visualization_div'));	    
+	    var chart = new google.visualization.AreaChart(document.getElementById('visualization_div'));
       var options = {
         width: 1000,
         height: 618,
@@ -21,8 +21,8 @@
 
         title: 'Game Genres',
         fontName: "Roboto",
-        "vAxis": {"title": "Year",
-				      "viewWindow" : {max : 1100}, 
+        "vAxis": {"title": "Number of Releases",
+				      "viewWindow" : {max : 1100},
 				      "textStyle" : { color: "black",
 						      fontName: "Roboto",
 						      fontSize: 12,
@@ -32,8 +32,8 @@
 							   fontName: "Roboto",
 							   fontSize: 16,
 							   bold: false,
-							   italic: false }},    
-			    "hAxis": {"title": "Number of Releases",
+							   italic: false }},
+			    "hAxis": {"title": "Year",
 				      "textStyle" : { color: "black",
 						      fontName: "Roboto",
 						      fontSize: 12,
@@ -47,32 +47,31 @@
         isStacked: "true"
       };
 
-	    
-      var query = "SELECT * FROM 1IOfn29P52Wvcvs3tpc-5S_aiOtCJEDXsi17lqdPR";	    
+
+      var query = "SELECT * FROM 1IOfn29P52Wvcvs3tpc-5S_aiOtCJEDXsi17lqdPR";
 	    var opts = {sendMethod: 'auto'};
 	    var queryObj = new google.visualization.Query('https://www.google.com/fusiontables/gvizdata?tq=', opts);
 	    var temp;
-      var data2;	
-    
+      var data2;
+
       queryObj.setQuery(query);
 
-      queryObj.send(function(e) { 
+      queryObj.send(function(e) {
 		    temp = e.getDataTable();
-        
+
         data2 = new google.visualization.DataView(temp);
-        
+
 		    chart.draw(data2, options);
     });
 
   }
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
 
-	    
+
+
+
+
+
+
+
+
+
